0fc09de2f3f60a37e94a009ec31c119ff80395f5
Вадим Фатеев <fateev1vv2@gmail.com>
2022-07-21 19:32:44 +0200

    1

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,31 +2,31 @@ import styles from "./Profile.module.css"
 
 export default function Profile({ username, tag, location, avatar, stats }) {
     
-    return <div className={styles.profile} class="profile">
-        <div className={styles.description} class="description">
+    return <div className={styles.profile} >
+        <div className={styles.description} >
             <img
             src={avatar}
                 alt="User avatar"
                 className={styles.avatar}
-            class="avatar"
+            
             />
-            <p className={styles.name} class="name">{username}</p>
-            <p className={styles.tag} class="tag">@{tag}</p>
-            <p className={styles.location} class="location">{location}</p>
+            <p className={styles.name} >{username}</p>
+            <p className={styles.tag} >@{tag}</p>
+            <p className={styles.location} >{location}</p>
         </div>
 
-        <ul className={styles.stats} class="stats">
+        <ul className={styles.stats} >
             <li>
-                <p className={styles.label} class="label">Followers</p>
-                <p className={styles.quantity} class="quantity">{stats.followers}</p>
+                <p className={styles.label} >Followers</p>
+                <p className={styles.quantity} >{stats.followers}</p>
             </li>
             <li>
-                <p className={styles.label} class="label">Views</p>
-                <p className={styles.quantity} class="quantity">{stats.views}</p>
+                <p className={styles.label} >Views</p>
+                <p className={styles.quantity} >{stats.views}</p>
             </li>
             <li>
-                <p className={styles.label} class="label">Likes</p>
-                <p className={styles.quantity} class="quantity">{stats.likes}</p>
+                <p className={styles.label} >Likes</p>
+                <p className={styles.quantity} >{stats.likes}</p>
             </li>
         </ul>
 </div>
