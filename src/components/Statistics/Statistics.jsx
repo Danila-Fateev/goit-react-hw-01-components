2db54349ce29587a380ea87f593906a6b4b1ea66
Вадим Фатеев <fateev1vv2@gmail.com>
2022-07-22 14:57:44 +0200

    random bg colors

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -4,13 +4,14 @@ function generateRandomColorHex() {
   return "#" + ("00000" + Math.floor(Math.random() * Math.pow(16, 6)).toString(16)).slice(-6);
 }
 
-export default function Statistics({props}) {
+export default function Statistics({ props }) {
+
     return (
         <section className={styles.statistics}>
   <h2 className={styles.title} >Upload stats</h2>
 
   <ul className={styles.stat_list}>
-{props.map(el => <li className={styles.item}  id={el.id}>
+{props.map(el => <li className={styles.item} style={{background: generateRandomColorHex()}}  id={el.id}>
     <p className={styles.label} >{el.label}</p>
     <p className={styles.percentage} >{el.percentage}%</p>
     </li>)}
