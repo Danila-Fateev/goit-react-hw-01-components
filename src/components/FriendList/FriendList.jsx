5939701f031125a95fe1e13b3e17c560f086a984
Вадим Фатеев <fateev1vv2@gmail.com>
2022-07-25 11:38:44 +0200

    Update FriendList.jsx

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -3,11 +3,19 @@ import styles from "./FriendList.module.css"
 export default function FriendList({ props }) {
     return (
         <ul className={styles.friend_list}>
-            {props.map(el => <li className={styles.item} id={el.id}>
-                <span className={el.isOnline ? styles.online : styles.offline}></span>
-                <img className={styles.avatar} src={el.avatar} alt={el.name} width="48" />
-                <p className={styles.name}>{el.name}</p>
-            </li>)}
+            <FriendListItem props={props} />
     </ul>
 )
+}
+
+function FriendListItem({ props }) {
+    return (
+        <>
+        {props.map(el => <li className={styles.item} id={el.id}>
+        <span className={el.isOnline ? styles.online : styles.offline}></span>
+        <img className={styles.avatar} src={el.avatar} alt={el.name} width="48" />
+        <p className={styles.name}>{el.name}</p>
+        </li>)}
+        </>
+    )
 }
