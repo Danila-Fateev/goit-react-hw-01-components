25afed369fd229edc59f2b4b09abe8292e3118d3
Вадим Фатеев <fateevdanon@gmail.com>
2022-07-28 10:18:28 +0200

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -32,7 +32,7 @@ export default function Statistics({ title, props }) {
 }
 
 Statistics.propTypes = {
-  title: PropTypes.string
+  title: PropTypes.string,
   props: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired,
